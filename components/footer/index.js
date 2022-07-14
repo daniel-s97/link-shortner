c087f0f937e3b1d4315e4508eed91222d7b88efe
Jacob <danielsock@gmail.com>
2022-07-14 13:32:14 -0700

    Finished header and footer

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,7 +1,14 @@
+import styles from "../../styles/footer.module.css";
+import Link from "next/link";
 export default function Footer() {
 	return (
-		<div>
-			<p>Footer</p>
+		<div className={styles.footer}>
+			<p>
+				Made by{" "}
+				<Link href="https://github.com/daniel-sockness">
+					<span className={styles.name}>Daniel Sockness</span>
+				</Link>{" "}
+			</p>
 		</div>
 	);
 }
