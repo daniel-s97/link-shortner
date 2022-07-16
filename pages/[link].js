0c6bc98ea2d6d526d1bbfbb4fbba08c1e83350c0
Jacob <danielsock@gmail.com>
2022-07-16 13:46:18 -0700

    View Handler and Link redirect

--- pages/[link].js
+++ pages/[link].js
@@ -1,12 +1,26 @@
 import { useRouter } from "next/router";
+import LinkHandler from "../lib/viewHandler";
 
-export default function Link() {
+function Link({ url }) {
 	const router = useRouter();
 	const { link } = router.query;
-
+	console.log(url);
+	router.push(url);
 	return (
 		<div>
-			<p>Link {link}</p>
+			<p>Short: {link}</p>
+			<p>Link: {url}</p>
 		</div>
 	);
 }
+
+export async function getServerSideProps(context) {
+	var { link } = context.query;
+	var url = await LinkHandler(link);
+	console.log(url);
+	return {
+		props: { url: url },
+	};
+}
+
+export default Link;
